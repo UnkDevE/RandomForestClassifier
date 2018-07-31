7727ab2a7a465f20154cc5e561904a9b7fe33c4c
Ethan Riley <riley787cmplex@gmail.com>
2018-07-31 19:36:55 +0100

    add information gain

--- DesisionTree.hs
+++ DesisionTree.hs
@@ -5,6 +5,8 @@ module DesisionTree
 )
 where
 
+import Data.List
+
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving Show
 
 data Feature = Continuous Double | Discrete String deriving (Eq, Show)
@@ -19,5 +21,16 @@ pmf trainingData feature value =
 
 entropy :: [[Feature]] -> Int -> Feature -> Double
 entropy trainingData feature value =
-    neg $ (pmf trainingData feature value) * logBase 2 $ pmf trainingData feature value
+    negate $ (pmf trainingData feature value) * (logBase 2 $ pmf trainingData feature value)
 
+featureEntropyProg :: (Feature -> Double -> Double) -> [[Feature]] -> Int -> Double
+featureEntropyProg f trainingData feature =
+    foldr (\x acc -> acc + (f x $ entropy trainingData feature x)) 0 $ nub $ column trainingData feature
+
+featureEntropy :: [[Feature]] -> Int -> Double
+featureEntropy = featureEntropyProg (\_ x -> x)
+
+informationGain :: ([[Feature]], Int) -> Int -> Double
+informationGain trainingData feature =
+    (featureEntropy (fst trainingData) $ snd trainingData) -
+    (featureEntropyProg (\x h -> h * (pmf (fst trainingData) feature x)) (fst trainingData) feature)
