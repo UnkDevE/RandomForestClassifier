2552ba02758a8195bbe51bba5b4a8356c58f08a1
Ethan Riley <riley787cmplex@gmail.com>
2018-08-01 17:24:03 +0100

    start ID3 algorithm

--- DesisionTree.hs
+++ DesisionTree.hs
@@ -7,8 +7,6 @@ where
 
 import Data.List
 
-data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving Show
-
 data Feature = Continuous Double | Discrete String deriving (Eq, Show)
 
 column :: [[a]] -> Int -> [a]
@@ -23,9 +21,12 @@ entropy :: [[Feature]] -> Int -> Feature -> Double
 entropy trainingData feature value =
     negate $ (pmf trainingData feature value) * (logBase 2 $ pmf trainingData feature value)
 
+types :: [[Feature]] -> Int -> [Feature]
+types trainingData feature = nub $ column trainingData feature
+
 featureEntropyProg :: (Feature -> Double -> Double) -> [[Feature]] -> Int -> Double
 featureEntropyProg f trainingData feature =
-    foldr (\x acc -> acc + (f x $ entropy trainingData feature x)) 0 $ nub $ column trainingData feature
+    foldr (\x acc -> acc + (f x $ entropy trainingData feature x)) 0 $ types trainingData feature
 
 featureEntropy :: [[Feature]] -> Int -> Double
 featureEntropy = featureEntropyProg (\_ x -> x)
@@ -34,3 +35,17 @@ informationGain :: ([[Feature]], Int) -> Int -> Double
 informationGain trainingData feature =
     (featureEntropy (fst trainingData) $ snd trainingData) -
     (featureEntropyProg (\x h -> h * (pmf (fst trainingData) feature x)) (fst trainingData) feature)
+
+getBestFeature :: ([[Feature]], Int) -> Int
+getBestFeature trainingData = foldr
+    (\i acc -> if informationGain trainingData acc < informationGain trainingData i then i else acc) 0
+    $ filter (/= snd trainingData) [1..((length $ head $ fst trainingData) - 1)]
+
+getContinousSplit :: [[Feature]] -> Int -> Feature
+getContinousSplit trainingData feature = foldr
+    (\i acc -> if entropy trainingData feature i < entropy trainingData feature acc then i else acc )
+    (head t) $ tail t
+    where t = types trainingData feature
+
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving Show
