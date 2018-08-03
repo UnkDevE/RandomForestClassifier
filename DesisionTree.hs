2878091ca90ce37e80c9fed152b5927fa8d69639
Ethan Riley <riley787cmplex@gmail.com>
2018-08-03 20:32:46 +0100

    fix errors

--- DesisionTree.hs
+++ DesisionTree.hs
@@ -2,16 +2,17 @@
 
 module DesisionTree
 (
-    Feature
-    DesisionTree
-    evaluateTree
+    Feature (..),
+    Tree (..),
+    evaluateTree,
     id3
 )
 where
 
 import Data.List (nub)
+import Data.Maybe (fromJust)
 
-data Feature = Continous Double | Discrete String deriving (Eq, Show)
+data Feature = Continous Double | Discrete String deriving (Eq, Ord, Show)
 
 column :: [[a]] -> Int -> [a]
 column grid index = foldr (\xs acc -> xs!!index:acc) [] grid
@@ -45,49 +46,50 @@ getBestFeature trainingData = foldr
     (\i acc -> if informationGain trainingData acc < informationGain trainingData i then i else acc) 0
     $ filter (/= snd trainingData) [1..((length $ head $ fst trainingData) - 1)]
 
-quicksort :: [(Feature, Double)] -> [(Feature, Double)]
+quicksort :: (Ord b) => [(a, b)] -> [(a, b)]
 quicksort (x:xs) =
     let smallerOrEqual = [a | a <- xs, snd a <= snd x]
         larger = [a | a <- xs, snd a > snd x]
     in quicksort smallerOrEqual ++ [x] ++ quicksort larger
 
-getSplitOrder :: [[Feature]] -> Int -> [Feature]
-getSplitOrder trainingData feature = fst $ unzip $ quicksort $ zip t $ map (entropy trainingData feature) t
+getSplitOrder :: [[Feature]] -> Int -> [(Feature, Int)]
+getSplitOrder trainingData feature =
+    zip (fst $ unzip $ quicksort $ zip t $ map (entropy trainingData feature) t) $ repeat feature
     where t = types trainingData feature
 
-data DesisionTree a = Node a [DesisionTree a] deriving Show
+data Tree a = Node a [Tree a] deriving (Eq, Show)
 
-data Crumb a = NCrumb a [DesisionTree a] [DesisionTree a] deriving Show
+data Crumb a = NCrumb a [Tree a] [Tree a] deriving (Eq, Show)
 
 type Breadcrumbs a = [Crumb a]
 
 type Zipper a = (Tree a, Breadcrumbs a)
 
-split Int -> [a] -> (a,a)
-split i xs = (filter (\a -> fst a < i) xs, filter (\a -> i > fst a))
+split :: Int -> [a] -> ([a], [a])
+split i xs = (snd $ unzip $ filter (\a -> fst a < i) zxs, snd $ unzip $ filter (\a -> i > fst a) zxs)
     where zxs = zip [1..] xs
 
-treeTo :: Zipper a -> Int -> Maybe Zipper a
+treeTo :: Zipper a -> Int -> Maybe (Zipper a)
 treeTo (Node x xs, bs) branch
-    | 0 < branch && branch < length xs = Just (xs!!branch, NCrumb x (fst splitxs) (snd splitxs))
+    | 0 < branch && branch < length xs = Just (xs!!branch, NCrumb x (fst splitxs) (snd splitxs):bs)
     | otherwise = Nothing
     where splitxs = split branch xs
 
-goRight :: Zipper a -> Maybe Zipper a
-goRight (item, NCrumb x ls (nitem:rs):bs) = Just (nitem, NCrumb x (x ls ++ [item]) rs)
+goRight :: Zipper a -> Maybe (Zipper a)
+goRight (item, NCrumb x ls (nitem:rs):bs) = Just (nitem, NCrumb x (ls ++ [item]) rs:bs)
 goRight (item, NCrumb x ls []:bs) = Nothing
 
-goUp :: Zipper a -> Maybe Zipper a
+goUp :: Zipper a -> Maybe (Zipper a)
 goUp (item, NCrumb x ls rs:bs) = Just (Node x (ls ++ [item] ++ rs), bs)
 goUp (item, []) = Nothing
 
-next :: Zipper a -> Maybe Zipper a
+next :: (Eq a) => Zipper a -> Maybe (Zipper a)
 next zipper
     | right /= Nothing = right
     | otherwise = goUp zipper
     where right = goRight zipper
 
-insertLevel Zipper a -> [a] -> Zipper a
+insertLevel :: Zipper a -> [a] -> Zipper a
 insertLevel (Node x _, bs) xs = (Node x (map (\x -> Node x []) xs), bs)
 
 isDiscrete :: Feature -> Bool
@@ -101,22 +103,23 @@ evaluate (features, out) (desision, feature)
 
 evaluateTree :: ([[Feature]], Int) -> Zipper (Feature, Int) -> ([[Feature]], Int)
 evaluateTree trainingData zipper@(Node x _, bs)
-    | up /= Nothing = getOutputSet eval up
+    | up /= Nothing = evaluateTree eval $ fromJust up
     | otherwise = eval
     where up = goUp zipper
           eval = evaluate trainingData x
 
 getOutputSet :: ([[Feature]],Int) -> Zipper (Feature, Int) -> [Feature]
-getOutputSet (feature, out) zipper = column (fst $ evaluateTree trainingData zipper) out
+getOutputSet trainingData@(feature, out) zipper =
+    column (fst $ evaluateTree trainingData zipper) out
 
 id3 :: ([[Feature]], Int) -> Zipper (Feature, Int) -> Zipper (Feature, Int)
-id3 (features, out) prevZipper@(Node x _, bs)
-    | length $ nub outSet == 1 = let newZipper = (Node x [Node (head outSet, out) []], bs)
-                                     nextZipper = next newZipper
-                                 in if nextZipper /= Nothing then id3 (features, out) nextZipper else newZipper
+id3 trainingData@(features, out) prevZipper@(Node x _, bs)
+    | (length $ nub outSet) == 1 = let newZipper = (Node x [Node (head outSet, out) []], bs)
+                                       nextZipper = next newZipper
+                                 in if nextZipper /= Nothing then id3 (features, out) (fromJust nextZipper) else newZipper
     | otherwise =
-        id3 (newFeatures, out)
-            $ insertLevel prevZipper $ getSplitOrder newFeatures $ getBestFeature newFeatures
+        id3 newFeatures
+            $ insertLevel prevZipper $ getSplitOrder (fst newFeatures) $ getBestFeature newFeatures
     where outSet = getOutputSet trainingData prevZipper
           splitF = split out features
-          newFeatures = (fst splitF) ++ outSet ++ (snd splitF)
+          newFeatures = ((fst splitF) ++ [outSet] ++ (snd splitF), out)
